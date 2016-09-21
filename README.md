34bde60441518ebdd82550ac73193f0ae85c0d69
Bretton Wade <bretton.wade@gmail.com>
2016-09-21 18:43:38 -0400

    switching to YUIDoc for documentation

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This project uses ant for building, with the "dev" target being the default:
 ### Build Dependencies
 * gcc (for the C-preprocessor)
 * uglifyjs (for Minification)
-* jsdoc (for Documentation)
+* yuidoc (for Documentation)
 
 #### UglifyJS2
 For ES6 compatibility, you have to use the "harmony" branch of UglifyJS2:
@@ -21,10 +21,14 @@ For ES6 compatibility, you have to use the "harmony" branch of UglifyJS2:
     git checkout harmony
     npm link .
     
-## JSDoc
- JSDoc seems to support ES6:
+## YUIDoc
+YUIDoc won me over because JSDoc tried very hard to enforce coding style, and simply wouldn't recognize
+the structure of my code as classes. YUIDoc just reads the comments, so I can structure my code how I 
+please:
  
-    npm install -g jsdoc
+    npm install -g yuidoc
+    
+   
 
 ### Building a scene
 ...
--- src/main/javascript/render.js
+++ src/main/javascript/render.js
@@ -1,8 +1,21 @@
 var context;
 var render;
+
+/**
+ * This is the description for my class.
+ *
+ * @class Render
+ */
 var Render = function () {
     var _ = Object.create (null);
 
+    /**
+     * The initializer for a rendering context.
+     *
+     * @method construct
+     * @param {String} canvasId the id of the canvas element to use for the rendering context
+     * @return {Render} Returns an initialized rendering context
+     */
     _.construct = function (canvasId) {
         var canvas = this.canvas = document.getElementById (canvasId);
         context = canvas.getContext ("webgl", { preserveDrawingBuffer: true });
@@ -12,6 +25,13 @@ var Render = function () {
         return this;
     };
 
+    /**
+     * Static method to create and construct a new rendering context.
+     *
+     * @method new
+     * @param {String} canvasId the id of the canvas element to use for the rendering context
+     * @return {Render} Returns an initialized rendering context
+     */
     _.new = function (canvasId) {
         return (render = Object.create (_).construct (canvasId));
     };
