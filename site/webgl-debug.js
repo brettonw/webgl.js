0424b8f716597561c34854cfd3a8ac6f2f76d432
Bretton Wade <bretton.wade@gmail.com>
2016-09-30 13:07:05 -0400

    default parameters for anisotropic filtering

--- site/webgl-debug.js
+++ site/webgl-debug.js
@@ -1368,6 +1368,7 @@ let Texture = function () {
     let _ = Object.create (null);
 
     let textures = Object.create (null);
+    let afExtension;
 
     _.construct = function (name, parameters) {
         this.name = name;
@@ -1380,12 +1381,12 @@ let Texture = function () {
             context.texImage2D (context.TEXTURE_2D, 0, context.RGBA, context.RGBA, context.UNSIGNED_BYTE, image);
             context.texParameteri (context.TEXTURE_2D, context.TEXTURE_MAG_FILTER, context.LINEAR);
             context.texParameteri (context.TEXTURE_2D, context.TEXTURE_MIN_FILTER, context.LINEAR_MIPMAP_LINEAR);
-            let extension = context.getExtension ("EXT_texture_filter_anisotropic");
-            let max = Math.min (context.getParameter(extension.MAX_TEXTURE_MAX_ANISOTROPY_EXT), 4);
-            context.texParameterf(context.TEXTURE_2D, extension.TEXTURE_MAX_ANISOTROPY_EXT, max);
+            context.texParameterf(context.TEXTURE_2D, afExtension.TEXTURE_MAX_ANISOTROPY_EXT, parameters.anisotropicFiltering);
             context.generateMipmap (context.TEXTURE_2D);
             context.bindTexture (context.TEXTURE_2D, null);
-            parameters.onReady (scope);
+            if ("onReady" in parameters) {
+                parameters.onReady (scope);
+            }
         };
         image.src = parameters.url;
 
@@ -1393,6 +1394,9 @@ let Texture = function () {
     };
 
     _.new = function (name, parameters) {
+        afExtension = (typeof afExtension !== "undefined") ? afExtension : function () { return context.getExtension ("EXT_texture_filter_anisotropic") } ();
+        // make sure anisotropic filtering is defined, and has a reasonable default value
+        parameters.anisotropicFiltering = Math.min (context.getParameter(afExtension.MAX_TEXTURE_MAX_ANISOTROPY_EXT), ("anisotropicFiltering" in parameters)? parameters.anisotropicFiltering : 4);
         return (textures[name] = Object.create (_).construct (name, parameters));
     };
 
