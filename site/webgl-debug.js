3654d162b4b7cb847fc1f5a0db087a3f1595ea22
Bretton Wade <brettonw.wade@gmail.com>
2016-11-24 00:41:00 -0500

    added text-file to the fetch architecture

--- site/webgl-debug.js
+++ site/webgl-debug.js
@@ -2624,6 +2624,32 @@ let Thing = function () {
 
     return _;
 } ();
+let TextFile = function () {
+    let _ = ClassNamed (CLASS_NAME_REQUIRED);
+
+    _.construct = function (parameters) {
+        LogLevel.say (LogLevel.INFO, "TextFile: " + parameters.name);
+
+        // there must be a url
+        if (typeof parameters.url === "undefined") throw "TextFile URL Required";
+
+        let scope = this;
+        let request = new XMLHttpRequest ();
+        request.onload = function (event) {
+            if (request.status === 200) {
+                scope.text = request.responseText;
+                // call the onReady handler if one was provided
+                if (typeof parameters.onReady !== "undefined") {
+                    parameters.onReady.notify (scope);
+                }
+            }
+        };
+        request.open ("GET", parameters.url);
+        request.send ();
+    };
+
+    return _;
+} ();
 let ShapeBuilder = function () {
     let _ = Object.create (null);
 
---
+++ src/main/javascript/core/text-file.js
@@ -0,0 +1,26 @@
+let TextFile = function () {
+    let _ = ClassNamed (CLASS_NAME_REQUIRED);
+
+    _.construct = function (parameters) {
+        LOG (LogLevel.INFO, "TextFile: " + parameters.name);
+
+        // there must be a url
+        if (typeof parameters.url === "undefined") throw "TextFile URL Required";
+
+        let scope = this;
+        let request = new XMLHttpRequest ();
+        request.onload = function (event) {
+            if (request.status === 200) {
+                scope.text = request.responseText;
+                // call the onReady handler if one was provided
+                if (typeof parameters.onReady !== "undefined") {
+                    parameters.onReady.notify (scope);
+                }
+            }
+        };
+        request.open ("GET", parameters.url);
+        request.send ();
+    };
+
+    return _;
+} ();
