a88f7b81d0e1f2624f2953a1f602f68ec38aedc3
Bretton Wade <brettonw.wade@gmail.com>
2016-12-07 12:44:15 -0500

    shaded-rgb fragment shader

--- site/shaders/fragment-rgb.glsl
+++ site/shaders/fragment-rgb.glsl
@@ -1,21 +1,9 @@
 precision highp float;
 
-uniform mat4 normalMatrix;
 uniform float outputAlpha;
-uniform vec3 cameraPosition;
 
 varying vec3 model;
-varying vec3 normal;
 
 void main(void) {
-	vec3 viewVector = normalize (cameraPosition - model);
-    vec3 normalVector = normalize ((normalMatrix * vec4 (normal, 0.0)).xyz);
-	float cosViewNormalAngle = clamp (dot(normalVector, viewVector), 0.0, 1.0);
-	float scale = 0.5 + (cosViewNormalAngle * 0.5);
-	/*
-    vec3 red = vec3 (1.0, 0.0, 0.0);
-    vec3 blue = vec3 (0.0, 0.0, 1.0);
-    gl_FragColor = vec4 ((red * cosViewNormalAngle) + (blue * (1.0 - cosViewNormalAngle)), outputAlpha);
-    */
-    gl_FragColor = vec4 (model * scale, outputAlpha);
+    gl_FragColor = vec4 (model, outputAlpha);
 }
