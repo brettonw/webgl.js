8dbd72a8bd014781cd7ab9e44dc0a27c3645090f
Bretton Wade <brettonw.wade@gmail.com>
2016-12-07 12:48:23 -0500

    tweaking the shader...

--- site/shaders/fragment-shaded-rgb.glsl
+++ site/shaders/fragment-shaded-rgb.glsl
@@ -12,10 +12,10 @@ void main(void) {
     vec3 normalVector = normalize ((normalMatrix * vec4 (normal, 0.0)).xyz);
 	float cosViewNormalAngle = clamp (dot(normalVector, viewVector), 0.0, 1.0);
 	float scale = 0.5 + (cosViewNormalAngle * 0.5);
-	/*
+
     vec3 red = vec3 (1.0, 0.0, 0.0);
     vec3 blue = vec3 (0.0, 0.0, 1.0);
     gl_FragColor = vec4 ((red * cosViewNormalAngle) + (blue * (1.0 - cosViewNormalAngle)), outputAlpha);
-    */
-    gl_FragColor = vec4 (model * scale, outputAlpha);
+
+    //gl_FragColor = vec4 (model * scale, outputAlpha);
 }
