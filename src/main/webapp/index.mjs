c9261be4bcd7f5c84fdc78cf0a950e6a1b1b4ce3
Bretton Wade <bretton.wade@gmail.com>
2022-06-15 00:28:08 -0400

    wrapper - minor fixes

--- src/main/webapp/index.mjs
+++ src/main/webapp/index.mjs
@@ -1,5 +1,5 @@
 // import all of wgl with a new instance...
-import {WebGL2, Utility, Float4x4, Float3} from "./webgl.mjs";
+import {WebGL2, Utility, Float3, Float4x4} from "./webgl.mjs";
 let wgl = WebGL2();
 Object.entries(wgl).forEach(([name, exported]) => window[name] = exported);
 
